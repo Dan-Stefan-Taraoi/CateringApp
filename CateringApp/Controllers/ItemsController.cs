2418a5991817e6e45d0559aaa46dd28faa712603
Dan-Stefan Taraoi <dan-stefan.taraoi@infoteam.de>
2026-06-02 08:58:48 +0200

    Add project files.

---
+++ CateringApp/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using CateringApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CateringApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult OverView()
+        {
+            var item = new Item() { Name = "Keyboard" };
+            return View(item);
+        }
+
+        /// <summary>
+        /// Placeholder of automative parameter fill.<br/>
+        /// Needs to be 'id'.
+        /// </summary>
+        /// <param name="id">Automatic parameter.</param>
+        /// <returns></returns>
+        public IActionResult Edit(int id)
+        {
+            return Content("Id is: " + id);
+        }
+    }
+}
---
+++ CateringApp/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace CateringApp.Models
+{
+    /// <summary>
+    /// This is equivalent to a table in a database.
+    /// It is a model that represents an item in the catering app.
+    /// It can be used to store information about the item such as its name, price, description, etc.
+    /// It can also be used to create relationships with other models such as orders, categories, etc.
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the item.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the item.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
